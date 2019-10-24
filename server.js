9718ab8c9567f03a880f054a8743aca7e65cca85
Mello de Assis <alexandre.mello.de.assis@scania.com>
2019-10-24 09:28:03 -0300

    Added project to GitHub

---
+++ server.js
@@ -0,0 +1,30 @@
+﻿const express = require('express'); //Framework de rotas e views
+const cors = require('cors');//Framework para disponibilizar acesso de todos os dominios, pode ser configurado
+const mongodb = require('mongodb'); //Framework de conexão ao MongoDB
+const mongoose = require('mongoose'); //ORM de conexão ao MongoDB
+const requireDir = require('require-dir'); //Utilizado para requerer arquivos de uma pasta especifica, nesse caso buscando os modulos criados
+
+//Inicinando o App
+const app = express();
+
+//Habilitar uso de json na aplicação
+app.use(express.json());
+
+//Habilitado para todos os dominios
+app.use(cors());
+
+//Connection to mongodb with mongoose
+mongoose.connect('mongodb+srv://mongodb:mongodb@cluster0-uj8sq.mongodb.net/nodeapi?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() =>
+        console.log('Conectou')).catch(err => console.log('Não conectou', err));
+
+//Busca  todos os models
+requireDir('./src/models');
+
+//Aponta arquivo de rotas e associa à aplicação
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+﻿const express = require('express');
+const routes = express.Router();
+
+
+const ProductController = require('./controllers/ProductController');
+
+routes.get('/product', ProductController.index);
+routes.get('/product/:id', ProductController.index);
+routes.post('/product/', ProductController.create);
+routes.put('/product/:id', ProductController.update);
+routes.delete('/product/:id', ProductController.delete);
+
+module.exports = routes;
